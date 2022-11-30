d166ee953ba7ea55d5668f88f76ccc642184e699
Rafaqat Ali <83506643+mianrafaqat@users.noreply.github.com>
2022-11-30 18:05:01 +0500

    new repo

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const storedLoggedInfo = localStorage.getItem("isLoggedIn")
+
+    if (storedLoggedInfo === '1') {
+      setIsLoggedIn(true)
+    }
+
+    setIsLoggedIn(true);
+    localStorage.setItem("isLogged", "1")
+  }, [])
+
+  const loginHandler = (email, password) => {
+   localStorage.setItem('isLoggedIn', '1')
+   setIsLoggedIn(true)
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn')
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
